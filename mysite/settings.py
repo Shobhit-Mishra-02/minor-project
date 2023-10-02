4f842619343570d04b90486559bd93ff5e8c6348
shobhit <shobhitmishra2002@gmail.com>
2023-10-02 09:00:45 +0530

    added a simple index.html page

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'voice_based_email.apps.VoiceBasedEmailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
